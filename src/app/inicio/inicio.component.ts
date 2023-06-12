615cae302c2b7ea83491fa7e7ba72f5c5e0710ad
Fernando Carvalho <nando_carvalhoo@hotmail.com>
2023-06-12 08:26:01 -0300

    subindo todo o projeto pela primeira vez

---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent {
+
+}
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+/**
+ * @title Toolbar overview
+ */
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+
+}
